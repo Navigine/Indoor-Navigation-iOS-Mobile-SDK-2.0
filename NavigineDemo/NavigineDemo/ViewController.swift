b97965c87736fa866cf3794cb75a288979484a18
PavelTychinin <p.tychinin@navigine.com>
2022-10-12 07:05:27 -0500

    change default server

--- NavigineDemo/NavigineDemo/ViewController.swift
+++ NavigineDemo/NavigineDemo/ViewController.swift
@@ -1,31 +1,31 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
+
     @IBOutlet weak var mTableView: UITableView!
-    
+
     var mLocationsList: [String] = []
-    
+
     var mNavigineSdk: NCNavigineSdk?
     var mLocationListManager: NCLocationListManager?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        mNavigineSdk = NCNavigineSdk.createInstance("4EE6-9A3F-F126-5FE6", server: "https://api.navigine.com")
-        
+
+        mNavigineSdk = NCNavigineSdk.createInstance("0000-0000-0000-0000", server: "https://ips.navigine.com")
+
         mLocationListManager = mNavigineSdk?.getLocationListManager()
-        
+
         mLocationListManager?.add(self)
-        
+
         self.mTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.mLocationsList.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:UITableViewCell = self.mTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath as IndexPath)
 
@@ -43,8 +43,8 @@ extension ViewController: NCLocationListListener {
         }
         self.mTableView.reloadData()
     }
-    
+
     func onLocationListFailed(_ error: Error?) {
-        
+
     }
 }
